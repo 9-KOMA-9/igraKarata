4706b508326947458b20588e69106a34124eb6dd
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-09-27 08:40:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igraKarte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int karta, zbrojkar = 0;
+            pocetak:
+            Console.WriteLine(" unesite kartu: ");
+            Console.WriteLine(" karta mora biti u rasponu od 1 do 13: ");
+            karta = Convert.ToInt32(Console.ReadLine());
+            if (karta >= 1 && karta <= 13)
+            {
+                zbrojkar = zbrojkar + karta;
+            }
+            else
+            {
+                Console.WriteLine(" karta nije u rasponu!! ");
+                goto pocetak;
+            }
+            if(zbrojkar < 31)
+            {
+                goto pocetak;
+            }
+            if(zbrojkar > 31)
+            {
+                Console.WriteLine(" zbroj je prešao 31! ");
+            }
+            if(zbrojkar == 31)
+            {
+                Console.WriteLine(" čestitamo pobjedili ste! ");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
